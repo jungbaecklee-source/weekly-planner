1c01a2c7f435f764378b686ada28166bd7c609d3
jungbaecklee-source <jungbaecklee@gmail.com>
2026-04-14 01:46:29 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "web-push": false,
+        "net": false,
+        "tls": false,
+        "fs": false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
